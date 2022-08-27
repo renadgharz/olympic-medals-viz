2fbd6d45d15433b0eda264f97f15266f388c3555
Renad <renad.gharz@outlook.com>
2022-08-27 10:46:59 -0600

    subsets by events

--- subsets.py
+++ subsets.py
@@ -39,3 +39,77 @@ europe = europe.reset_index(drop=True)
 oceania = master_data[master_data['region'] == 'Oceania']
 oceania = oceania.reset_index(drop=True)
 
+# subsetting for summer competitions only
+summer_asia = asia[['countries', 'summer_participations', 'summer_gold', 'summer_silver', 'summer_bronze',
+                    'summer_total']]
+
+summer_africa = africa[['countries', 'summer_participations', 'summer_gold', 'summer_silver', 'summer_bronze',
+                        'summer_total']]
+
+summer_middle_east = middle_east[['countries', 'summer_participations', 'summer_gold', 'summer_silver', 'summer_bronze',
+                                  'summer_total']]
+
+summer_south_america = south_america[['countries', 'summer_participations', 'summer_gold', 'summer_silver',
+                                      'summer_bronze', 'summer_total']]
+
+summer_north_america = north_america[['countries', 'summer_participations', 'summer_gold', 'summer_silver',
+                                      'summer_bronze', 'summer_total']]
+
+summer_central_america = central_america[['countries', 'summer_participations', 'summer_gold', 'summer_silver',
+                                          'summer_bronze', 'summer_total']]
+
+summer_europe = europe[['countries', 'summer_participations', 'summer_gold', 'summer_silver', 'summer_bronze',
+                        'summer_total']]
+
+summer_oceania = oceania[['countries', 'summer_participations', 'summer_gold', 'summer_silver', 'summer_bronze',
+                          'summer_total']]
+
+# subsetting for winter competitions only
+winter_asia = asia[['countries', 'winter_participations', 'winter_gold', 'winter_silver', 'winter_bronze',
+                    'winter_total']]
+
+winter_africa = africa[['countries', 'winter_participations', 'winter_gold', 'winter_silver', 'winter_bronze',
+                        'winter_total']]
+
+winter_middle_east = middle_east[['countries', 'winter_participations', 'winter_gold', 'winter_silver', 'winter_bronze',
+                                  'winter_total']]
+
+winter_south_america = south_america[['countries', 'winter_participations', 'winter_gold', 'winter_silver',
+                                      'winter_bronze', 'winter_total']]
+
+winter_north_america = north_america[['countries', 'winter_participations', 'winter_gold', 'winter_silver',
+                                      'winter_bronze', 'winter_total']]
+
+winter_central_america = central_america[['countries', 'winter_participations', 'winter_gold', 'winter_silver',
+                                          'winter_bronze', 'winter_total']]
+
+winter_europe = europe[['countries', 'winter_participations', 'winter_gold', 'winter_silver', 'winter_bronze',
+                        'winter_total']]
+
+winter_oceania = oceania[['countries', 'winter_participations', 'winter_gold', 'winter_silver', 'winter_bronze',
+                          'winter_total']]
+
+# subsetting for total competitions
+total_asia = asia[['countries', 'total_participation', 'total_gold', 'total_silver', 'total_bronze',
+                   'total_total']]
+
+total_africa = africa[['countries', 'total_participation', 'total_gold', 'total_silver', 'total_bronze',
+                       'total_total']]
+
+total_middle_east = middle_east[['countries', 'total_participation', 'total_gold', 'total_silver', 'total_bronze',
+                                 'total_total']]
+
+total_south_america = south_america[['countries', 'total_participation', 'total_gold', 'total_silver', 'total_bronze',
+                                     'total_total']]
+
+total_north_america = north_america[['countries', 'total_participation', 'total_gold', 'total_silver', 'total_bronze',
+                                     'total_total']]
+
+total_central_america = central_america[['countries', 'total_participation', 'total_gold', 'total_silver',
+                                         'total_bronze', 'total_total']]
+
+total_europe = europe[['countries', 'total_participation', 'total_gold', 'total_silver', 'total_bronze',
+                       'total_total']]
+
+total_oceania = oceania[['countries', 'total_participation', 'total_gold', 'total_silver', 'total_bronze',
+                         'total_total']]
